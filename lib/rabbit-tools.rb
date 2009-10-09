084736a579dbb9af0579d25a921be885368d10dd
Stefan Saasen <stefan@coravy.com>
2009-10-09 21:59:21 +1300

    Injected version information (0.2.1)

--- lib/rabbit-tools.rb
+++ lib/rabbit-tools.rb
@@ -30,7 +30,7 @@ Dir[File.join(File.dirname(__FILE__), 'rabbit-tools', '**', '*.rb')].sort.each {
 module RabbitMQ
   
   class CLI # :nodoc:
-    VERSION = "0.2.0"
+    VERSION = "0.2.1"
     
     def initialize
       @out = STDOUT
